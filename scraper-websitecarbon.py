34a6509fe712b080e491bac24b04eca608f72548
machine <i.g.ivanov@student.vu.nl>
2022-04-14 13:28:23 +0200

    Update scraper-websitecarbon.py

--- scraper-websitecarbon.py
+++ scraper-websitecarbon.py
@@ -20,8 +20,26 @@ time.sleep(1)
 
 driver.find_element(By.ID, 'js-new-test-button').click()
 
-driver.implicitly_wait(6)
+time.sleep(6)
 
-text = driver.find_element(By.CLASS_NAME, "js-countup-content").text
+text = driver.find_element(By.XPATH, "/html/body/div/main/article/div[1]/div/div[2]/p/span/span/span").text
+time.sleep(1)
+driver.execute_script("window.scrollBy(0,500)","")
+time.sleep(1)
+co2 = driver.find_element(By.XPATH, "/html/body/div/main/article/div[2]/div[2]/p/span/span/span").text
+time.sleep(1)
+
+energy = driver.find_element(By.XPATH, "/html/body/div/main/article/div[3]/div[2]/p/span").text
+
+driver.execute_script("window.scrollBy(0,1000)","")
+time.sleep(1)
+co2equiv = driver.find_element(By.XPATH, "/html/body/div/main/article/div[4]/div[1]/div[2]/div/h4/span[1]").text
+
 print(text)
+print("___")
+print(co2)
+print("___")
+print(energy)
+print("___")
+print(co2equiv)
 time.sleep(10000);
