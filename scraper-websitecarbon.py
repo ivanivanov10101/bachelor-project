2159d7e67c7100e8ddf07eaff08b645bac5ff87c
machine <i.g.ivanov@student.vu.nl>
2022-04-14 13:04:49 +0200

    Update scraper-websitecarbon.py

--- scraper-websitecarbon.py
+++ scraper-websitecarbon.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 site = "https://www.google.com/"
@@ -10,10 +14,14 @@ driver.get("https://www.websitecarbon.com/")
 
 driver.maximize_window()
 
-driver.find_element_by_id('wgd-cc-url').send_keys(site)
+driver.find_element(By.ID, 'wgd-cc-url').send_keys(site)
 
-time.sleep(5)
+time.sleep(1)
 
-driver.find_element_by_id('js-new-test-button').click()
+driver.find_element(By.ID, 'js-new-test-button').click()
 
+time.sleep(1)
+
+text = driver.find_element(By.CLASS_NAME, "js-countup-content").text
+print(text)
 time.sleep(10000);
