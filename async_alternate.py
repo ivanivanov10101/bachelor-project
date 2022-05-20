7a26029488717333242cae0f3c478ed6d7c8b35f
ivanivanov10101 <78234071+ivanivanov10101@users.noreply.github.com>
2022-05-20 11:19:03 +0200

    62.4k parsed, 50004 usable

--- async_alternate.py
+++ async_alternate.py
@@ -25,7 +25,7 @@ def parse(htmls):
 
     data.insert(0, ['URL', 'Green Hosting', 'Bytes', 'Cleaner Than %', 'Stats_Adjusted Bytes', 'Stats_Energy', 'Stats_CO2_Grid_Grams', 'Stats_CO2_Grid_Litres', 'Stats_CO2_Renewable_Grams', 'Stats_CO2_Renewable_Litres'])
 
-    with open('csv\\database59800-60000.csv', "w", newline="") as f:
+    with open('csv\\database62200-62400.csv', "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
 
